3f747ba52e9f881ad7d13d7b256b09de66753c3b
mitroio <mitroio@uni.coventry.ac.uk>
2018-11-26 18:17:01 +0000

    Created and displayed the form

---
+++ src/components/account/Login/LoginForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TextInput from '../../common/formElements/TextInput'
+
+const LoginForm = ({onChange, onSubmit, loading, errors, user}) => (
+    <form name='loginForm'>
+        <TextInput
+        value={user.username}
+        onChange={onChange}
+        error={errors.username}
+        name='username'
+        label='Username'
+        placeholder='Enter username...'
+        type='text' />
+
+        <TextInput
+        value={user.password}
+        onChange={onChange}
+        error={errors.password}
+        name='password'
+        label='Password'
+        placeholder='Enter password'
+        type='password' />
+    </form>
+)
+LoginForm.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired,
+    errors: PropTypes.arrayOf(PropTypes.any),
+    user: PropTypes.object.isRequired
+}
+export default LoginForm
--- src/components/account/Login/LoginPage.js
+++ src/components/account/Login/LoginPage.js
@@ -1,12 +1,87 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {Redirect} from 'react-router-dom'
+import LoginForm from './LoginForm'
+import * as userActions from '../../../actions/userActions'
+import toastr from 'toastr'
 
 export class LoginPage extends React.Component {
+    constructor(props,context){
+        super(props,context)
+        this.state = {
+            user: Object.assign({},this.props.user),
+            loading: false,
+            redirect: false,
+            errors: []
+        }
+        this.onStateUpdate = this.onStateUpdate.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    componentDidMount() {
+        console.log('Mounted LoginPage')
+    }
+    componentWillUnmount() {
+        console.log('Unmounting')
+    }
+    async onStateUpdate(event) {
+        const field = event.target.name
+        const user = Object.assign({},this.state.user)
+
+        user[field] = event.target.value
+        await this.setState({user})
+        return
+    }
+    onSubmit(event) {
+        event.preventDefault()
+        this.setState({loading: true})
+
+        this.props.actions.loginUser(this.state.user)
+            .then(() => this.redirect())
+            .catch(err => {
+                toastr.error(err.message)
+                this.setState({loading: false})
+            })
+    }
+    redirect() {
+        this.setState({
+            loading: false,
+            redirect: true
+        })
+        toastr.success(`Logged in as ${this.state.user.username}`)
+    }
     render() {
+        if (this.state.redirect) {
+            return (
+                <Redirect to={`/user/${this.state.user.username}/hub`} />
+            )
+        }
         return (
-            <div>Login Page</div>
+            <div className='container'>
+                <LoginForm
+                onChange = {this.onStateUpdate}
+                onSubmit = {this.onSubmit}
+                loading = {this.state.loading}
+                errors = {this.state.errors}
+                user = {this.state.user} />
+            </div>
             )
     }
 }
+LoginPage.propTypes = {
+    user: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired
+}
+function mapStateToProps(state,ownProps) {
+    return {
+        user: state.user
+    }
+}
+function mapDispatchToProps(dispatch){
+    return {
+        actions: bindActionCreators(userActions,dispatch)
+    }
+}
 
-export default connect(null)(LoginPage)
+export default connect(mapStateToProps,mapDispatchToProps)(LoginPage)
