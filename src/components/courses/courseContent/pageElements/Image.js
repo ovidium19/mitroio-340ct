55dfeff4bbd13244212e8857320359d5238534cc
mitroio <mitroio@uni.coventry.ac.uk>
2018-11-28 23:57:04 +0000

    Had to move images to static folder to be served properly

--- src/components/courses/courseContent/pageElements/Image.js
+++ src/components/courses/courseContent/pageElements/Image.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 const Image = ({content, klass, index}) => {
     return (
-        <div className='image-container d-flex justify-content-center align-items-center'>
-            <img src={`/static${content.href}`} alt={content.subtitle} className={klass} />
+        <div className='image-container d-flex justify-content-center align-items-center flex-column'>
+            <img src={`${content.href}`} alt={content.subtitle} className={klass} />
             <p className='img-subtitle text-center text-muted'>{content.subtitle}</p>
         </div>
     )
